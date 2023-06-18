16cefa7fb0832b7d413b5b6c2aaf97e2a2606a55
torondil <bulavskipavel@gmail.com>
2023-06-18 19:02:17 +0200

    fix: fix bugs

--- index.js
+++ index.js
@@ -57,16 +57,12 @@ app.post('/web-data', async (req, res) => {
             type: 'article',
             id: queryId,
             title: 'Успешная покупка',
-            input_message_content: {message_text: 'Поздравляю с покупкой, вы приобрели товар на сумму ' + totalPrice}
+            input_message_content: {
+                message_text: `Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice} ${products.map(item => item.title).join(', ')}`,
+            }
         })
         return res.status(200).json({})
-    } catch {
-        await bot.answerWebAppQuery(queryId, {
-            type: 'article',
-            id: queryId,
-            title: 'Не удалось приобрести товар',
-            input_message_content: {message_text: 'Не удалось приобрести товар'}
-        })
+    } catch (e) {
         return res.status(500).json({})
     }
 })
